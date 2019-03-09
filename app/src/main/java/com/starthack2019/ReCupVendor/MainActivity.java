fced61f70e3bfe54130dc3adf9aea1c16eaebb25
shelest <el.rache@gmail.com>
2019-03-09 09:17:35 +0100

    Init

--- app/src/main/java/com/starthack2019/ReCupVendor/MainActivity.java
+++ app/src/main/java/com/starthack2019/ReCupVendor/MainActivity.java
@@ -2,6 +2,7 @@ package com.starthack2019.ReCupVendor;
 
 import android.os.Bundle;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -21,13 +22,47 @@ import com.starthack2019.ReCupVendor.barcode.BarcodeCaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private static final int BARCODE_READER_REQUEST_CODE = 1;
+    private TextView mResultTextView;
+    String UserBarcode = "";
+    String CupBarcode = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
+        Button ScanUserBarcode_btn = (Button) findViewById(R.id.scan_user_barcode_button);
+        Button ScanCupBarcode_btn = (Button) findViewById(R.id.scan_cup_barcode_button);
+        Button Execute_btn = (Button) findViewById(R.id.execute_button);
+        mResultTextView = (TextView) findViewById(R.id.result_textview);
+
+        ScanUserBarcode_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), BarcodeCaptureActivity.class);
+                startActivityForResult(intent, BARCODE_READER_REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == BARCODE_READER_REQUEST_CODE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
+                    Point[] p = barcode.cornerPoints;
+                    mResultTextView.setText(barcode.displayValue);
+                } else mResultTextView.setText(R.string.no_barcode_captured);
+            } else Log.e(LOG_TAG, String.format(getString(R.string.barcode_error_format),
+                    CommonStatusCodes.getStatusCodeString(resultCode)));
+        } else super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
