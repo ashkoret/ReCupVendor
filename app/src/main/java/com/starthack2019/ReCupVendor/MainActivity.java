d0eceebd2117ec52f7b9f32566ccfa19b726728f
shelest <el.rache@gmail.com>
2019-03-09 14:29:06 +0100

    Init

--- app/src/main/java/com/starthack2019/ReCupVendor/MainActivity.java
+++ app/src/main/java/com/starthack2019/ReCupVendor/MainActivity.java
@@ -18,10 +18,20 @@ import android.widget.TextView;
 
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.vision.barcode.Barcode;
+
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+import java.net.URISyntaxException;
+
+import org.json.JSONObject;
+import org.json.JSONException;
+import com.google.gson.*;
+
 import com.starthack2019.ReCupVendor.barcode.BarcodeCaptureActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private Socket socket;
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     private static final int BARCODE_READER_REQUEST_CODE = 1;
     private TextView mResultTextView;
@@ -37,6 +47,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
+        Button Remove_btn = (Button) findViewById(R.id.scan_cup2remove_barcode_button);
+        Remove_btn.setOnClickListener(this);
         Button ScanUserBarcode_btn = (Button) findViewById(R.id.scan_user_barcode_button);
         ScanUserBarcode_btn.setOnClickListener(this);
         Button ScanCupBarcode_btn = (Button) findViewById(R.id.scan_cup_barcode_button);
@@ -45,19 +57,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Execute_btn.setOnClickListener(this);
         mResultTextView = (TextView) findViewById(R.id.result_textview);
 
-        /*ScanUserBarcode_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), BarcodeCaptureActivity.class);
-                startActivityForResult(intent, BARCODE_READER_REQUEST_CODE);
-            }
-        });*/
     }
 
     @Override
     public void onClick(View v) {
 
         switch (v.getId()) {
+
+            case R.id.scan_cup2remove_barcode_button:
+                Intent intent_cup_remove = new Intent(getApplicationContext(), BarcodeCaptureActivity.class);
+                startActivityForResult(intent_cup_remove, BARCODE_READER_REQUEST_CODE);
+                CupBarcode = mResultTextView.getText().toString();
+                break;
 
             case R.id.scan_cup_barcode_button:
                 Intent intent_cup = new Intent(getApplicationContext(), BarcodeCaptureActivity.class);
@@ -72,7 +83,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.execute_button:
-                // do your code
+                JSONObject codes = new JSONObject();
+                Gson gson = new Gson();
+                String JsonString = "";
+                try {
+                    codes.accumulate("user_QRcode", UserBarcode);
+                    codes.accumulate("cup_QRcode", CupBarcode);
+                    JsonString = gson.toJson(codes);
+                }
+                catch(JSONException f){
+                    f.printStackTrace();
+                }
+                try {
+                    socket = IO.socket("http://yourlocalIPaddress:3000");
+                    socket.connect();
+                }
+                 catch (URISyntaxException e) {
+                     e.printStackTrace();
+                 }
+                socket.emit("qrcodes", JsonString);
+
                 break;
 
             default:
